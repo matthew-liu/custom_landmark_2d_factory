1cac2ed6cf81591451d14d52a20a2a65c7ce379d
liux44 <liux44@cs.washington.edu>
2017-05-25 16:31:40 -0700

    update landmark_2d to include publisher + subscriber

--- src/landmark_2d.cpp
+++ src/landmark_2d.cpp
@@ -4,40 +4,70 @@
 #include <opencv2/opencv.hpp>
 #include "matching.h"
 
-int main( int argc, char** argv ) {
-  // Load image and template
-  Mat img = imread( argv[1], 1 );
-  Mat templ = imread( argv[2], 1 );
+ros::Publisher* pub;
+Mat templ;
 
-  if ( !img.data ) {
-      printf("No image data \n");
-      return -1;
-  }
+void matcher(const sensor_msgs::Image::ConstPtr& msg);
+
+int main( int argc, char** argv ) {
+  // Load template from stdin
+  templ = imread( argv[1], 1 );
+
   if ( !templ.data ) {
       printf("No template data \n");
       return -1;
   }
 
-  list<Point> lst;
+  ros::init(argc, argv, "landmark_2d");
 
-  if (matching(img, templ, lst)) {
-    printf("\n\nmatching success! \n");
+  ros::NodeHandle n;
+
+  ros::Publisher pub_instance = n.advertise<sensor_msgs::Image>("output_channel", 1000);
+  pub = &pub_instance;
+
+  ros::Subscriber sub = n.subscribe("/head_mount_kinect/rgb/image_raw", 1000, matcher);
+
+  // wait ~3 secs for each image processing
+  ros::Rate loop_rate(0.3);
+
+  while (ros::ok()) {
+    ros::spinOnce();
+    loop_rate.sleep();
   }
 
+  return 0;
+}
 
-  printf("--------------\n REPEAT Matched Points Info:\n\n");
-  printf("coordinates: %d, %d\n", x_dist, y_dist);
-  // display matched parts
-  for (list<Point>::iterator it = lst.begin(); it != lst.end(); it++) {
-    printf("position: %d, %d\n", it->x, it->y);
+void matcher(const sensor_msgs::Image::ConstPtr& msg) {
+  ROS_INFO("scene img received sucessfully");
+
+  if (ros::ok()) {
+    cv_bridge::CvImagePtr cv_ptr;
+
+    try {
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8); // , sensor_msgs::image_encodings::RGB8
+    } 
+    catch (cv_bridge::Exception& e) {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      exit(1);
+    }
+
+    // run matching on the image
+    list<Point> lst;
+    bool result = matching(cv_ptr->image, templ, lst);
+
+    if (!result) {
+      ROS_INFO("--No matching for the template exists in current scene--\n");
+      return;
+    }
+    
+    // printf("--------------\n REPEAT Matched Points Info:\n\n");
+    // printf("coordinates: %d, %d\n", x_dist, y_dist);
+    // // display matched parts
+    // for (list<Point>::iterator it = lst.begin(); it != lst.end(); it++) {
+    //   printf("position: %d, %d\n", it->x, it->y);
+    // }
+
+    pub->publish(cv_ptr->toImageMsg());
   }
-
-  char const * image_window = "Source Image";
-
-  namedWindow(image_window, CV_WINDOW_NORMAL);
-  resizeWindow(image_window, 600,600);
-
-  imshow( image_window, img );
-
-  waitKey(0);
 }
