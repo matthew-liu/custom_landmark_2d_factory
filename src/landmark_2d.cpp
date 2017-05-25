388aa6f67c15418a9e57a2338f570278edba5533
liux44 <liux44@cs.washington.edu>
2017-05-25 14:42:13 -0700

    add matching.h & landmark_2d skeleton

---
+++ src/landmark_2d.cpp
@@ -0,0 +1,43 @@
+#include "ros/ros.h"
+#include "sensor_msgs/Image.h"
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+#include "matching.h"
+
+int main( int argc, char** argv ) {
+  // Load image and template
+  Mat img = imread( argv[1], 1 );
+  Mat templ = imread( argv[2], 1 );
+
+  if ( !img.data ) {
+      printf("No image data \n");
+      return -1;
+  }
+  if ( !templ.data ) {
+      printf("No template data \n");
+      return -1;
+  }
+
+  list<Point> lst;
+
+  if (matching(img, templ, lst)) {
+    printf("\n\nmatching success! \n");
+  }
+
+
+  printf("--------------\n REPEAT Matched Points Info:\n\n");
+  printf("coordinates: %d, %d\n", x_dist, y_dist);
+  // display matched parts
+  for (list<Point>::iterator it = lst.begin(); it != lst.end(); it++) {
+    printf("position: %d, %d\n", it->x, it->y);
+  }
+
+  // char const * image_window = "Source Image";
+
+  // namedWindow(image_window, CV_WINDOW_NORMAL);;
+  // resizeWindow(image_window, 600,600);
+
+  // imshow( image_window, img );
+
+  // waitKey(0);
+}
---
+++ src/matching.h
@@ -0,0 +1,177 @@
+#ifndef MATCHING_H
+#define MATCHING_H
+
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <list>
+
+using namespace std;
+using namespace cv;
+
+int match_method = CV_TM_CCOEFF_NORMED;
+// max #times allowed for resizing
+int counter = 3;
+// the threshold for BEST acceptable matching points
+float raw_match_limit = 0.7;
+// the threshold for ALL acceptable matching points
+float match_limit = 0.63;
+
+int x_dist, y_dist;
+
+
+double matching_method(Mat& img, Mat& templ, list<Point>& matching);
+bool around(int x, int y, Point& p);
+bool around_matching_points(int x, int y, list<Point>& matching, Point** point);
+
+
+bool matching(Mat& img, Mat& templ, list<Point>& lst) {
+
+  double first_value = matching_method(img, templ, lst);
+  // success without rescaling
+  if (first_value > raw_match_limit) return true;
+
+  Mat scaled_templ;
+
+  double value;
+  
+  // scale down
+  resize(templ, scaled_templ, Size(), 0.9, 0.9, INTER_AREA);
+  value = matching_method(img, scaled_templ, lst);
+
+  if (value > raw_match_limit) return true;
+  if (value > first_value) {
+    double factor = 0.8;
+    while (counter > 0) {
+      resize(templ, scaled_templ, Size(), factor, factor, INTER_AREA);
+      value = matching_method(img, scaled_templ, lst);
+      if (value > raw_match_limit) return true;
+      factor -= 0.1;
+      counter--;
+    }
+  } else {
+    // scale up
+    resize(templ, scaled_templ, Size(), 1.1, 1.1, INTER_LINEAR);
+    value = matching_method(img, scaled_templ, lst);
+    if (value > raw_match_limit) return true;
+    if (value > first_value) {
+      double factor = 1.2;
+      while (counter > 0) {
+        resize(templ, scaled_templ, Size(), factor, factor, INTER_AREA);
+        value = matching_method(img, scaled_templ, lst);
+        if (value > raw_match_limit) return true;
+        factor += 0.1;
+        counter--;
+      }
+    }
+  }
+
+  printf("**No matching for the template exist!**\n");
+  return false;
+}
+
+
+double matching_method(Mat& img, Mat& templ, list<Point>& matching) {
+
+  y_dist = (int) templ.rows;
+  x_dist = (int) templ.cols;
+
+  printf("\n--------\n");
+  printf("x_dist: %d, y_dist: %d\n\n", x_dist, y_dist);
+
+  // Create the result matrix
+  Mat result;
+
+  int result_cols =  img.cols - templ.cols + 1;
+  int result_rows = img.rows - templ.rows + 1;
+
+  result.create( result_rows, result_cols, CV_32FC1 );
+
+  // Do the Matching
+  matchTemplate( img, templ, result, match_method );
+
+  double minVal; double maxVal; Point minLoc; Point maxLoc;
+  minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, Mat() );
+
+  printf("best match value: %f, position: %d, %d\n", result.at<float>(maxLoc), maxLoc.x, maxLoc.y);
+
+  if (maxVal < raw_match_limit) {
+    return maxVal;
+  }
+
+  matching.push_back(maxLoc);
+
+  // scan through result to find all matching points
+  int i,j;
+  float* p;
+  for( i = 0; i < result.rows; i++) {
+      p = result.ptr<float>(i);
+      for ( j = 0; j < result.cols; j++) {
+          if ( p[j] > match_limit) { // acceptable matching point
+            Point* point_ptr;
+            if (around_matching_points(j, i, matching, &point_ptr)) {
+              if (p[j] > result.at<float>(*point_ptr)) { // current point has better score
+                point_ptr->x = j;
+                point_ptr->y = i;
+              }
+            } else {
+              matching.push_back(Point(j, i));
+            }
+          }
+      }
+  }
+
+
+  printf("--------------\nMatched Points Info:\n\n");
+
+
+  // display matched parts
+  for (list<Point>::iterator it = matching.begin(); it != matching.end(); it++) {
+    printf("point intensity: %f, position: %d, %d\n", result.at<float>(*it), it->x, it->y);
+
+    rectangle( img, *it, Point( it->x + templ.cols , it->y + templ.rows ), Scalar(255, 255, 0), 10, 8, 0 );
+    rectangle( result, *it, Point( it->x + templ.cols , it->y + templ.rows ), Scalar(255, 255, 0), 10, 8, 0 );
+  }
+ 
+  // Create windows & show images
+  // char const * image_window = "Source Image";
+  // char const * result_window = "Result window";
+
+  // namedWindow(image_window, CV_WINDOW_NORMAL);
+  // namedWindow(result_window, CV_WINDOW_NORMAL);
+  // resizeWindow(image_window, 600,600);
+  // resizeWindow(result_window, 600,600);
+
+  // imshow( image_window, img );
+  // imshow( result_window, result );
+
+  // waitKey(0);
+  return maxVal;
+}
+
+
+// check whether point(x, y) is around point p
+bool around(int x, int y, Point& p) {
+  if (abs(p.x - x) < x_dist && abs(p.y - y) < y_dist)
+    return true;
+  return false;
+}
+
+
+// check whether point(x, y) is around ANY point in the matching list
+bool around_matching_points(int x, int y, list<Point>& matching, Point** point) {
+  if (matching.empty())
+    return false;
+
+  for (list<Point>::iterator it = matching.begin(); it != matching.end(); it++) {
+    if (around(x, y, *it)) {
+      *point = &(*it);
+      return true;
+    }
+  }
+  return false;
+}
+
+#endif // MATCHING_H
