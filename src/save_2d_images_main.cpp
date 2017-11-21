3e79512a1043ea8a5f461a6f4f361d0de70088c2
liux44 <liux44@cs.washington.edu>
2017-11-20 16:31:45 -0800

    finish rgbd_fetcher

--- src/save_2d_images_main.cpp
+++ src/save_2d_images_main.cpp
@@ -7,21 +7,29 @@
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
 #include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
 
 using namespace sensor_msgs;
 using namespace message_filters;
 
-std::string* filename_ptr;
-
-void callback(const ImageConstPtr& rgb, const ImageConstPtr& depth) {
+void callback(const ImageConstPtr& rgb, const ImageConstPtr& depth, std::string& filename) {
   ROS_INFO("received 2 images");
+
+  // fetch CameraInfo
+  sensor_msgs::CameraInfoConstPtr camera_info =
+      ros::topic::waitForMessage<sensor_msgs::CameraInfo>(
+          "/head_camera/rgb/camera_info");
+
+  ROS_INFO("received camear_info");  
+
   rosbag::Bag bag;
-  bag.open(*filename_ptr, rosbag::bagmode::Write);
+  bag.open(filename, rosbag::bagmode::Write);
   bag.write("/head_camera/rgb/image_raw", ros::Time::now(), rgb);
   bag.write("/head_camera/depth_registered/image_raw", ros::Time::now(), depth);
+  bag.write("/head_camera/rgb/camear_info", ros::Time::now(), camera_info);
   bag.close();
 
-  std::exit(0);
+  ros::shutdown(); 
 }
 
 void print_usage() {
@@ -36,18 +44,18 @@ int main(int argc, char** argv) {
   }
   
   std::string filename(std::string(argv[1])  + ".bag");
-  filename_ptr = &filename;
 
   ros::init(argc, argv, "save_2d_images_main");
 
-  ros::NodeHandle nh;
+  ros::NodeHandle nh;  
+
   message_filters::Subscriber<Image> rgb_sub(nh, "/head_camera/rgb/image_raw", 1);
   message_filters::Subscriber<Image> depth_sub(nh, "/head_camera/depth_registered/image_raw", 1);
 
   typedef sync_policies::ApproximateTime<Image, Image> SyncPolicy;
   // ApproximateTime takes a queue size as its constructor argument, hence SyncPolicy(10)
   Synchronizer<SyncPolicy> sync(SyncPolicy(10), rgb_sub, depth_sub);
-  sync.registerCallback(boost::bind(&callback, _1, _2));
+  sync.registerCallback(boost::bind(&callback, _1, _2, filename));
 
   ros::spin();
 
